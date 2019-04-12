cf6952e4742d98c16ccb1a1fbea7cc4a2497a645
Vasilios Magriplis <etameson@gmail.com>
2019-04-12 23:14:37 +0930

    * Start using std::filesystem

--- src/.cpp/main.cpp
+++ src/.cpp/main.cpp
@@ -2,6 +2,10 @@
 #include "../.h/class.h"
 #include <iostream>
 #include <cstddef>
+#include <filesystem>
+
+using std::filesystem::path;
+using std::filesystem::directory_entry;
 
 int main(int argc, char* arg[])
 {
@@ -10,6 +14,21 @@ int main(int argc, char* arg[])
 	std::cout << "Src: " << progArgs.SrcPath().data() << std::endl;
 	std::cout << "Dst: " << progArgs.DstPath().data() << std::endl;
 	std::cout << "Del: " << progArgs.Delim().data() << std::endl;
-
+	
+	try
+	{
+		std::error_code error;
+		path src(progArgs.SrcPath());
+		directory_entry dirEntry(src, error);
+		if(dirEntry.exists())
+			std::cout << "Located" << std::endl;
+		else
+			std::cout << "Not located " << error.message() << std::endl;
+	}
+	catch (std::filesystem::filesystem_error err)
+	{
+		std::cout << err.what() << std::endl;
+	}
+	
 	return 0;
 }
